cd1f058e7ba0e6e2952afa03942f684e527c1f15
IsratErin <131592678+IsratErin@users.noreply.github.com>
2025-09-24 11:32:22 +0200

    server init

--- Week_5/server.ts
+++ Week_5/server.ts
@@ -1,3 +1,16 @@
 import express from "express";
 
+//initialization of the app
 const app = express();
+
+const PORT = 3000;
+
+//set up a basic route
+app.get(`/`, (req, res) => {
+  res.send("Welcome to the page!");
+});
+
+//start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
+});
